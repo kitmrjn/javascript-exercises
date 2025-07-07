f13b518239daaeb38d18a750384adfe910f60b83
Kit Bagasol <mjbolwar@gmail.com>
2025-07-07 21:57:16 +0800

    I assigned values to the pre defined variables

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -8,7 +8,14 @@
 */
 
 // Add your code right below, good luck!
+const firstName = 'Carlos';
+const lastName = 'Stevenson';
+const thisYear = 1965;
+const birthYear = 1947;
 
+let greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old.";
+let fullName = firstName + " " + lastName;
+let age = thisYear - birthYear;
 
 
 
