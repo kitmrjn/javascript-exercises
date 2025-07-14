f3b211a1a08cc1e7daf41a924ab451257cbcd9c0
Kit Bagasol <mjbolwar@gmail.com>
2025-07-14 20:41:25 +0800

    yeah I cheated hehe

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, number) {
+    let string = '';
+    for (let i = 0; i < number; i++) {
+        string += word;
+    }
+    return string;
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
