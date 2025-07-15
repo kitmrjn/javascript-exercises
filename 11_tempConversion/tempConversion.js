da16289321671705ef464a45f88f3a4b514ec3cb
Kit Bagasol <mjbolwar@gmail.com>
2025-07-15 21:10:12 +0800

    Again I didn't peek at the solution and I
    Still got the same results

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const convertedCelsius = (fahrenheit - 32) * 5/9;
+  const roundedToOneDecimal = Math.round(convertedCelsius * 10) / 10;
+  return roundedToOneDecimal;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const convertedFahrenheit = celsius * (9 / 5) + 32;
+  const roundedToOneDecimal = Math.round(convertedFahrenheit * 10) / 10;
+  return roundedToOneDecimal;
 };
 
 // Do not edit below this line
