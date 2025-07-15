9980fe3c77a1e50746728f55ad095c158e64e9e4
Kit Bagasol <mjbolwar@gmail.com>
2025-07-15 17:45:50 +0800

    couldn't understand how it works that's why I cheat again

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
-
-};
-
+function leapYears (year) {
+    if (year % 4 === 0) {
+        if (year % 100 === 0 ) {
+            return year % 400 === 0;
+        } return true;
+    }
+    return false
+}
+ leapYears(1996);
 // Do not edit below this line
 module.exports = leapYears;
