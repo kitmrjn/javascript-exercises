282ff3424dabe5d08eeaad56732c240c08d24a02
Kit Bagasol <mjbolwar@gmail.com>
2025-07-15 20:47:48 +0800

    The solution i came up with is
    Somehow different with the given solution
    But it still does the job

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,11 +1,10 @@
 function leapYears (year) {
-    if (year % 4 === 0) {
-        if (year % 100 === 0 ) {
-            return year % 400 === 0;
-        } return true;
-    }
-    return false
-}
+   if (year % 4 === 0 && year % 100 != 0 || year % 400 == 0) {
+    return true;
+   } else {
+    return false;
+   }};
+   
  leapYears(1996);
 // Do not edit below this line
 module.exports = leapYears;
