660be2f7d59175ccde3aa92d2bba0227287e365a
Kit Bagasol <mjbolwar@gmail.com>
2025-07-15 16:48:52 +0800

    got pretty confused so I peeked and just tried to understand what the code does

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-};
+function removeFromArray (array, ...args) {
+        let result = [];
 
+        array.forEach((index) => {
+            if (!args.includes(index)) {
+                result.push(index);
+            } 
+        });
+            return result;
+    };
+    
 // Do not edit below this line
 module.exports = removeFromArray;
