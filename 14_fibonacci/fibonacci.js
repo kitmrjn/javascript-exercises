5011fc04ed19a89952d3430155cf0cdc9cb50d92
Kit Bagasol <mjbolwar@gmail.com>
2025-07-31 04:16:42 +0800

    This shit was the hardest of all
    I mean math is hard as hell
    I was confused with how am I going to store the previous and current number
    Luckily google was here

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let numString = Number(number);
+    let previous = 0;
+    let current = 1;
 
+    if (numString < 0) {
+        return "OOPS";
+    } else if (numString != 0) {
+        for (let i = 2; i <= numString; i++) {
+            const next = previous + current;
+            previous = current;
+            current = next;
+        };
+        return current;
+    };  
+    return 0;
 };
 
 // Do not edit below this line
