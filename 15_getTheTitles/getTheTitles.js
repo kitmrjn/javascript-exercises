32bcf547f3d2495e83bcefbc98a3e3af96fbf930
Kit Bagasol <mjbolwar@gmail.com>
2025-07-31 03:25:29 +0800

    This one was alot easier than the fibonacci

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(libro) {
+    let titles = []
+    for( let i = 0; i < libro.length; i++) {
+         titles.push(libro[i].title);
+    };
+    return titles;
 };
 
 // Do not edit below this line
