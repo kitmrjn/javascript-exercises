9c4df71f14bcc67d21eeb8b4a5ed3711d95ceeda
Kit Bagasol <mjbolwar@gmail.com>
2025-07-30 23:12:15 +0800

    this shit was hard asf
    Had to google lots of string methods just to get this right
    
    ignoring the punctuation and spaces is harder too hahaha
    luckily there is stackoverflow

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.toUpperCase().replace(/[^a-zA-Z0-9]/g, '');
 
+    let palindrome = string.split("").reverse().join("");
+
+    if (palindrome == string) {
+        return true;
+    } 
+        return false;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
