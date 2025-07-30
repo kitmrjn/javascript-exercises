f6c49a4561cf60cb68684cd976eff81f388bdf72
Kit Bagasol <mjbolwar@gmail.com>
2025-07-30 21:50:58 +0800

    Did not have any hard time except
    The last, I was able to figured out what method to use
    But then i was wrong with the decalration
    I did not have a initial value of 1
    That's why I keep getting 0 hahaha

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const total =  array.reduce((sum, array) => sum + array, 0);
+  return total
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const total = array.reduce((currentTotalValue, currentValue) => currentTotalValue * currentValue);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return total = Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let total = 1
+	for (let i = 1; i <= number; i++) {
+     total *= i;
+  };
+  return total;
 };
 
 // Do not edit below this line
