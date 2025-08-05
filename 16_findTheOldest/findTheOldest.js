723b0345ffbe2d5c96bfe359983fbd0f9be542f8
Kit Bagasol <mjbolwar@gmail.com>
2025-08-06 03:22:04 +0800

    peeked a little, cause I was really struglling

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const getAge = function (birth, death) {
+    if (!death) {
+        death = 2025;
+    };
+    return death - birth;
+};
 
+
+const findTheOldest = function(people) {
+    return people.reduce((age, person) => {
+        const oldestAge = getAge(age.yearOfBirth,age.yearOfDeath);
+        const currentAge = getAge(person.yearOfBirth, person.yearOfDeath);
+        return oldestAge < currentAge  ? person : age;
+    });
 };
 
 // Do not edit below this line
